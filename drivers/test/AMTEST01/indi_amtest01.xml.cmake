de480550bab81b48b923a57803d5cbfede597df6
Roman Dvořák <romandvorak@astrometers.eu>
2025-08-24 16:57:44 +0000

    repair missing xml file build

---
+++ drivers/test/AMTEST01/indi_amtest01.xml.cmake
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+  <devGroup group="Test">
+    <device label="AMTEST01" 
+            manufacturer="Astrometers"
+            mdpd="true">
+      <driver name="AMTEST01">indi_amtest01</driver>
+      <version>@AMTEST01_VERSION_MAJOR@.@AMTEST01_VERSION_MINOR@</version>
+    </device>
+  </devGroup>
+</driversList>
